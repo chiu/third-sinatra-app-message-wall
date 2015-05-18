92576a5e82fb06d67de75667e2e06767260fd859
chiu <chiucode@gmail.com>
2015-05-18 03:03:13 +0000

    now can see other posts by same guy

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,12 +8,29 @@ class Message < ActiveRecord::Base
 
 
   def other_messages(current_author)
-        message_record = Message.where(author: current_author)
+    message_record = []
+    output_string = ""
+    message_record = Message.where(author: current_author)
 
-        message_record[0]
+    # message_record[0]
 
-        return message_record[0]
-     
+    # return message_record[0]
+
+
+    message_record.each do |element|
+      output_string << " next message: \n" << element.content
+
+
+
+
+    end
+
+    return output_string
+
   end
+
+
+
+
 
 end
