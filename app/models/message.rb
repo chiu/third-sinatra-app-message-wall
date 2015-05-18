50a766c0263df6a0b0937aa472336730c2f284f3
chiu <chiucode@gmail.com>
2015-05-18 02:50:05 +0000

    preparing to add author search

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,14 @@ class Message < ActiveRecord::Base
   validates :content, presence: true, length: { maximum: 140 }
   validates :author, presence: true, length: { maximum: 25 }
 
+
+  def other_messages(current_author)
+        message_record = Message.where(author: current_author)
+
+        message_record[0]
+
+        return message_record[0]
+     
+  end
+
 end
