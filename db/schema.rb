728d9b0887e03eca5122abb154c48e9cc0aa5874
chiu <chiucode@gmail.com>
2015-05-18 02:32:03 +0000

    added stamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518015546) do
+ActiveRecord::Schema.define(version: 20150518022355) do
 
   create_table "messages", force: true do |t|
-    t.string "title"
-    t.string "author"
-    t.string "content"
-    t.string "url"
+    t.string   "title"
+    t.string   "author"
+    t.string   "content"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
